fb9de059e0946fc066e1b9858eecd18da101ec4a
nasrullah <nhussaini@luc.edu>
2021-03-31 07:13:55 -0700

    Header completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-  <Navbar /> 
+    <>
+    <Particles 
+      className="particles-canvas"
+      params={{
+        particles:{
+          number:{
+            value:20,
+            density:{
+              enable:true,
+              value_area:900
+            
+            }
+          },
+          shape:{
+            type:"star",
+            stroke:{
+              width:5,
+              color: "#f9ab00"
+            }
+
+          }
+        }
+      }}
+    />
+    <Navbar /> 
+    <Header />
+  </>
   );
 }
 
